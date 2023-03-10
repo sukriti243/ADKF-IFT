08a52a2244151626b18b2766138fa941b47fb35e
sukriti243 <64130895+sukriti243@users.noreply.github.com>
2023-03-10 10:41:32 +0000

    change alpha

--- fs_mol/utils/neumann_hypergradient.py
+++ fs_mol/utils/neumann_hypergradient.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 def hypergradient(query_loss, support_loss, params_outer, params_inner):
     
@@ -7,6 +8,11 @@ def hypergradient(query_loss, support_loss, params_outer, params_inner):
     
     # dL(train)/dp(inner)
     dtrain_dinner = torch.autograd.grad(support_loss, params_inner, retain_graph=True, create_graph=True)
+    # for v in dtrain_dinner:
+    #     if v==None:
+    #         print(v)
+    #     else:
+    #         print('---not None---')
     
     # p = dL(val)/dp(inner) * H^{-1}
     p = approxInverseHVP(v, dtrain_dinner, params_inner)
@@ -18,13 +24,27 @@ def hypergradient(query_loss, support_loss, params_outer, params_inner):
 
 def approxInverseHVP(v, dtrain_dinner, params_inner):
     p = v  # dL(val)/dp(inner)
-    alpha = 1e-2
+    alpha = 1e-4
+    p_vec = cat_list_to_tensor(p)
+
+    hessian = torch.autograd.grad(dtrain_dinner, params_inner, grad_outputs=v, retain_graph=True)
+    # print("check:", [_h.shape for _h in hessian])
+    # for alpha in [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]:
+    #     prt_stuff = []
+    #     for _h, _v in zip(hessian, v):
+    #         _h, _v = torch.flatten(_h), torch.flatten(_v)
+    #         mat = _v*torch.ones_like(_h) - alpha*_h
+    #         prt_stuff.append(torch.norm(mat)/np.sqrt(len(mat)))
+    #     print("alpha: ", alpha, prt_stuff)
 
     for j in range(5):
-        
+        p_prev_vec = p_vec
         grad = torch.autograd.grad(dtrain_dinner, params_inner, grad_outputs=v, retain_graph=True)
         v = [v_ - alpha * g for v_, g in zip(v, grad)]
         p = [p_ + v_ for p_, v_ in zip(p, v)]
+        p_vec = cat_list_to_tensor(p)
+    #     print(torch.norm(p_vec-p_prev_vec), torch.norm(p_vec), j)
+    # breakpoint()
 
     return [alpha*pp for pp in p]
 
